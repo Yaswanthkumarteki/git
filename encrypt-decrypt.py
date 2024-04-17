7554a2aed013a74c00da3c6eb582ff4ea3a0a531
Yaswanthkumarteki <140479654+Yaswanthkumarteki@users.noreply.github.com>
2024-04-17 21:20:13 +0530

    Update encrypt-decrypt.py
    added code

--- encrypt-decrypt.py
+++ encrypt-decrypt.py
@@ -1,1 +1,42 @@
+def encrypt(FileName, password):
+    passcode = len(password)+(len(password)%2)
+    EncryptedPWD = ""
+    EncryptedData = ""
 
+    for i in password:
+        EncryptedPWD += chr(ord(i) + passcode)
+
+    with open(FileName,'r') as p:
+        FileContent = p.read()
+    with open(FileName,'w', encoding='utf-8') as p:
+        for i in FileContent:
+            EncryptedData += chr(ord(i) + passcode)
+        p.write(EncryptedData)
+    with open(FileName,'a', encoding='utf-8') as p:
+        p.write(EncryptedPWD)
+
+
+def decrypt(FileName,password):
+    EncryptedPWD = ""
+    content = ""
+    passcode = len(password) + (len(password)%2)
+    for i in password:
+        EncryptedPWD += chr(ord(i) + passcode)
+    with open(FileName, 'r', encoding = 'utf-8') as p:
+        FileContent = p.read()
+
+    if FileContent[-len(EncryptedPWD):] == EncryptedPWD:
+        with open(FileName, 'w', encoding='utf-8') as p:
+            EncryptedData = FileContent
+            for i in EncryptedData:
+                content += chr(ord(i) - passcode)
+            content = content[: -(len(password))]
+            p.write(content)
+
+    else:
+        with open(FileName, 'w', encoding = 'utf-8') as p:
+            p.write(FileContent)
+        raise Exception("Sorry you entered wrong password")
+    
+if __name__ == '__main__':
+    print("In encrypt-decrypt file")
